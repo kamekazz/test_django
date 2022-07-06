f32812d9449d98c7a51bf6bd141cc60364f4ebdb
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-07-05 22:02:46 -0400

    model v1

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,24 @@
+from turtle import title
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.URLField(null=True, blank=True)
+    source_link = models.URLField(null=True, blank=True)
+    created = models.DateField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True,
+        primary_key=True, editable=False
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+# class Review(models.Model):
+#     owner=
+#     project=
