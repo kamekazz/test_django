8d8e76b427e635f1e224eacea3ec1942aec1835e
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-07-06 20:47:55 -0400

    set model

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,3 @@
-from turtle import title
 import uuid
 from django.db import models
 
@@ -10,6 +9,9 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.URLField(null=True, blank=True)
     source_link = models.URLField(null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateField(auto_now_add=True)
     id = models.UUIDField(
         default=uuid.uuid4, unique=True,
@@ -19,6 +21,33 @@ class Project(models.Model):
     def __str__(self) -> str:
         return self.title
 
-# class Review(models.Model):
-#     owner=
-#     project=
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    # owner=
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True,
+        primary_key=True, editable=False
+    )
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True,
+        primary_key=True, editable=False
+    )
+
+    def __str__(self) -> str:
+        return self.name
--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.projectsPage, name='projects'),
     path('projects/', views.projectsPage, name='projects'),
     path('project/<str:pk>', views.projectPage, name='project'),
 ]
