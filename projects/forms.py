4047e43ee270206fcdc35c500090ac54ade47ba3
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-07-12 18:24:41 -0400

    teams

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,7 @@
-from django.forms import ModelForm
-from .models import Project
+from django.db.models.base import Model
+from django.forms import ModelForm, widgets
+from django import forms
+from .models import Project, Review
 
 
 class ProjectForm(ModelForm):
@@ -13,3 +15,13 @@ class ProjectForm(ModelForm):
             'tags',
             'featured_image'
         ]
+
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
