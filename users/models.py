5655030c343677bbbfd9754323c2cf89d5a98c3b
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-07-12 19:28:48 -0400

    model user set up

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
-from django.db import models
 
+import uuid
+from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
+    short_intro = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    profile_image = models.ImageField(
+        blank=True, null=True, upload_to='profiles/', default="profiles/user-default.png")
+    social_github = models.URLField(max_length=200, blank=True, null=True)
+    social_twitter = models.URLField(max_length=200, blank=True, null=True)
+    social_youtube = models.URLField(max_length=200, blank=True, null=True)
+    social_website = models.URLField(max_length=200, blank=True, null=True)
+    social_linkedin = models.URLField(max_length=200, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+
+    def __str__(self) -> str:
+        return str(self.user.username)
