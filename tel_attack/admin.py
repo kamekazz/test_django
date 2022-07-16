941954efebea71e6fc65f10b5c28632dc6997008
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-07-16 03:00:22 -0400

    module

--- tel_attack/admin.py
+++ tel_attack/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Attack, TelephoneNumber
+admin.site.register(Attack)
+admin.site.register(TelephoneNumber)
--- tel_attack/models.py
+++ tel_attack/models.py
@@ -1,3 +1,34 @@
+import uuid
 from django.db import models
 
+from users.models import Profile
+
 # Create your models here.
+
+
+class Attack(models.Model):
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=200)
+    telephone = models.ManyToManyField('TelephoneNumber', blank=True)
+    created = models.DateField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True,
+        primary_key=True, editable=False
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class TelephoneNumber(models.Model):
+    name = models.CharField(max_length=200)
+    target_number = models.CharField(max_length=9)
+    created = models.DateField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True,
+        primary_key=True, editable=False
+    )
+
+    def __str__(self) -> str:
+        return self.name
