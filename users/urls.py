cd181ed0fea3696d0a97548d70f885d924f056c4
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-07-15 22:44:59 -0400

    login setup

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.profilesPage, name='profiles'),
 
     path('login/', views.loginUser, name='login'),
+    path('register/', views.registerUser, name='register'),
     path('logout/', views.logoutUser, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
+
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, authenticate, logout
 # Create your views here.
 from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 
 
 def profilesPage(req):
@@ -12,7 +14,7 @@ def profilesPage(req):
 
 
 def loginUser(request):
-
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -31,11 +33,30 @@ def loginUser(request):
         else:
             messages.error(request, 'Username OR password is incorrect')
 
-    context = {}
+    context = {'page': page}
     return render(request, 'pages/login_register.html', context)
 
 
 def logoutUser(req):
     logout(req)
-    messages.info(request, 'User was logged out!')
+    messages.info(req, 'User was logged out!')
     return redirect('login')
+
+
+def registerUser(req):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if req.method == 'POST':
+        form = CustomUserCreationForm(req.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(req, 'User account was created!')
+            login(req, user)
+            return redirect('profiles')
+        else:
+            messages.error(req, 'An error has occurred during reg')
+    context = {'page': page, 'form': form}
+    return render(req, 'pages/login_register.html', context)
